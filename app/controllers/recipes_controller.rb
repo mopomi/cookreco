2da68b52dc9a4f4e07ad7c88984d4322435f70ca
mopomi <mrooooo.33@gmail.com>
2020-12-03 12:03:55 +0900

    Active Storageの導入

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,5 +8,14 @@ class RecipesController < ApplicationController
 
   def create
   end
-  
+
+
+
+
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :image, :genre_id).merge(user_id: current_user.id)
+  end
 end
